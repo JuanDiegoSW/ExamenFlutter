86a8e76e29478e34fce16b4ea51c454c8231d69a
JuanDiegoSW <juan.lazaro.m@tecsup.edu.pe>
2021-11-02 11:36:36 -0500

    error en el listado, mejorando

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -19,16 +19,14 @@ class _ListaPageState extends State<ListaPage> {
   @override
   void initState() {
     super.initState();
-    _agregar10();
+    _agregarimagenes();
 
 
     _scrollController.addListener(() {
 
       if( _scrollController.position.pixels == _scrollController.position.maxScrollExtent ) {
-        // _agregar10();
-        fetchData();
+        _agregarimagenes();
       }
-
     });
 
   }
@@ -50,7 +48,6 @@ class _ListaPageState extends State<ListaPage> {
       body: Stack(
         children: <Widget>[
             _crearLista(),
-            _crearLoading()
         ],
       )
       
@@ -63,7 +60,7 @@ class _ListaPageState extends State<ListaPage> {
     return RefreshIndicator(
 
         onRefresh: obtenerPagina1,
-
+      
         child: ListView.builder(
         controller: _scrollController,
         itemCount: _listaNumeros.length,
@@ -72,7 +69,7 @@ class _ListaPageState extends State<ListaPage> {
           final imagen = _listaNumeros[index];
 
           return FadeInImage(
-            image: NetworkImage('https://picsum.photos/500/300/?image=$imagen'),
+            image: NetworkImage('https://picsum.photos/500/300/?image=0'),
             placeholder: AssetImage('assets/jar-loading.gif'),
           );
         },
@@ -88,78 +85,19 @@ class _ListaPageState extends State<ListaPage> {
 
       _listaNumeros.clear();
       _ultimoItem++;
-      _agregar10();
+      _agregarimagenes();
 
     });
-
-    return Future.delayed(duration);
-
   }
 
 
 
-  void _agregar10() {
+  void _agregarimagenes() {
 
-    for (var i = 1; i < 10; i++) {
-      _ultimoItem++;
-      _listaNumeros.add( _ultimoItem );
-    }
+    _ultimoItem++;
+    _listaNumeros.add( _ultimoItem );
 
     setState(() {});
-
+  
   }
-
-
-
-  Future<Null> fetchData() async {
-
-    _isLoading = true;
-    setState(() {});
-
-    final duration = new Duration( seconds: 2 );
-    return new Timer( duration, respuestaHTTP );
-
-  }
-
-  void respuestaHTTP() {
-
-    _isLoading = false;
-
-    _scrollController.animateTo(
-      _scrollController.position.pixels + 100,
-      curve: Curves.fastOutSlowIn,
-      duration: Duration( milliseconds: 250)
-    );
-
-
-
-    _agregar10();
-
-  }
-
-  Widget _crearLoading() {
-
-    if ( _isLoading ) {
-      return Column(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircularProgressIndicator()
-            ],
-          ),
-          SizedBox( height: 15.0)
-        ],
-      );
-      
-      
-      
-    } else {
-      return Container();
-    }
-
-  }
-
 }
